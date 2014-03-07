b1a5e4ca1f5c54e2d63654ad44c29cb76c6ea04c
Pablo Ruan <ruanmyv@gmail.com>
2014-03-07 08:58:41 -0300

    Novos exemplos.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ node-examples
 =============
 
 A set of node.js examples.
+
+
+Chat app was found at:
+https://github.com/tonyblundell/simple-node-socket.io-chat
+
+Express app was found at:
+https://github.com/clocklimited/BasicExpressSite
---
+++ chat-app/server.js
@@ -0,0 +1,51 @@
+var clients = [];
+
+
+var http = require("http");
+var fs = require("fs");
+var httpServer = http.createServer(function(request, response) {
+    
+    fs.readFile(__dirname + "/client.html", "utf8", function(error, content) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.end(content);
+    });
+
+}).listen(process.env.PORT || 1337);
+
+
+
+var io = require("socket.io").listen(httpServer);
+io.sockets.on("connection", function(socket) {
+    socket.on('join', function(nick, callback) {
+
+        if (clients.indexOf(nick) < 0) {
+
+            socket.nick = nick;
+            
+            clients.push(nick);
+
+            socket.broadcast.emit("user-joined", nick);
+        
+            callback(true, clients);
+        
+        } else {
+            callback(false);
+        }
+    });
+
+    
+    socket.on("chat", function(message) {
+        if (socket.nick && message) {
+            io.sockets.emit("chat", {sender: socket.nick, message: message});
+        }
+    });
+
+
+    socket.on("disconnect", function() {
+        if (socket.nick) {
+            clients.splice(clients.indexOf(socket.nick), 1);
+            io.sockets.emit("user-left", socket.nick);
+        }
+    });
+
+});
